623fd118d3c0810fc6a62800da6af621f983a217
JuanFornieles <jfornielestiburcio01@gmail.com>
2026-03-28 18:00:26 +0100

    Update que-es.php

--- api/que-es.php
+++ api/que-es.php
@@ -1,1 +1,64 @@
+<!--- 
+Titulo = Función XLX
+Creador = JFT
+Fecha = 20/01/2024
+--->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org">
+<HTML>
+<HEAD>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<TITLE>Página Centrada</TITLE>
+<STYLE type="text/css">
+    html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        background-color: #ffffff;
+        font-family: Verdana, Geneva, sans-serif;
+    }
+    .muestraCentradoSinCentrado {
+        display: flex;
+        justify-content: center;
+        align-items: center; 
+        height: 100%;
+        width: 100%;
+    }
+    .textoEnLineaBotoneraXLXPreparaRecurso {
+        text-align: center;
+        padding: 20px;
+    }
+</STYLE>
+</HEAD>
+<body>
+
+    <DIV CLASS="muestraCentradoSinCentrado">
+        <DIV CLASS="textoEnLineaBotoneraXLXPreparaRecurso">
+            <h1>¿Qué es XLX?</h1>
+            <p>XLX es un intérprete (traductor) de código que interpreta a HTML, css y un poco de JavaScript de manera simple.</p>
+            <p>Las principales funciones de este son las nombradas aquí: </p>
+  <SMALL> 1. Aprender HTML rápido </SMALL>
+  <SMALL> 2. Crear código sin saber inglés </SMALL>
+  <SMALL> 3. Poder interpretar el código y posteriormente copiarlo para usarlo en tu web sin saber HTM </SMALL>
+            <p> 
+        </div>
+    </div>
+
+
+
+</BODY>
+</html>
 
